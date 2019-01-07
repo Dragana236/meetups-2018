407a9f7199c9b1a19444f8031e32a304465cccd2
Dragana236 <draganap236@gmail.com>
2019-01-07 13:23:25 +0100

    Add changes

--- survey_analysis.R
+++ survey_analysis.R
@@ -4,36 +4,25 @@ library(ggplot2)
 library(tidyr)
 library(forcats)
 
-
 # Load the dataset
 maha <- read_csv('162maha.csv')
-
-
 
 # Data Cleaning
 head(maha)
 
 glimpse(maha)
 
-
-
 maha <- maha %>% 
   select(c(2, 3, 4), 8:14, 24:26)
 
 maha
-
-
 
 # Change all the character columns to factors #
 maha <- maha %>% 
   mutate_if(is.character, as.factor)
 
 
-maha
-
-
 # Make a two column dataset with variable names and number of levels MOZDA
-
 maha_levels <- maha %>% 
   summarise_all(nlevels)
 
@@ -49,7 +38,6 @@ maha %>%
   pull(University) %>% 
   levels()
 
-
 # EDA
 # How many males and Females are in the dataset?
 maha %>%  # in general there are more Females
@@ -61,11 +49,9 @@ maha <- maha %>%
   filter(Gender != 'Other') %>%
   droplevels()
 
-
 maha %>%   # much more Female students
   ggplot(aes(x  = Gender)) +
   geom_bar()
-
 
 maha %>% 
   count(Major)
@@ -78,27 +64,17 @@ ggplot(maha, aes(x = Major)) +
   geom_bar() + 
   coord_flip()
 
-
-# ili 
+# or 
 ggplot(maha, aes(x = Major)) + 
   geom_bar() +
   theme(axis.text.x = element_text(angle = 90))
 
-
-# Make a bar plot with reordering a variable by its frequency
-
-
-
-
 # 2-way contingency table
-
 table(maha$Gender, maha$Major)
 
 ggplot(maha, aes(x = Gender, fill = Major)) + 
   geom_bar(position = "dodge") +
   ylab("counts")
-
-
 
 # Conditional proportions
 options(scipen = 999, digits = 3) 
@@ -111,15 +87,10 @@ prop.table(table(maha$Gender, maha$Major), 1)
 #conditionals on columns
 prop.table(table(maha$Gender, maha$Major), 2)
 
-
-
-
 # Plot proportion of gender, conditional on align
 ggplot(maha, aes(x = Gender, fill = Major)) + 
   geom_bar(position = "fill") +
   ylab("proportion")
-
-
 
 
 ggplot(maha, aes(x = Major, fill = Gender)) + 
@@ -128,34 +99,23 @@ ggplot(maha, aes(x = Major, fill = Gender)) +
   theme(axis.text.x = element_text(angle = 90))
 
 
-# Nase pitanje
-
-# MOJ KOD
-maha
-
+# Are girls more confident than boys?
 maha_tidy <- maha %>% 
   select(1, 4:13) %>% 
   gather(my_key, my_value, -Gender) %>% 
   mutate(secure = ifelse(my_value %in% c(4,5), 1, 0)) 
 
-
-
-
 maha_summary <- maha_tidy %>% filter(!is.na(secure)) %>% 
   group_by(Gender) %>% 
   summarise(average = mean(secure))
 
-
+# If...else statement
 my_value <- c(1, 2, 3, 4, 5) 
 if ((my_value == 4)｜(my_value == 5)) {
   print('1')
 } else {
   print('0')
 }
-
-
-
-
 
 maha %>% 
   select(c(1,3), 4:13) %>% 
@@ -169,16 +129,8 @@ maha %>%
   facet_wrap(~Major)
 
 
-
-
-
-
-
-
 maha_fem <- maha %>% 
   filter(Gender == 'Female')
-
-
 
 
 # Ordering one variable by another
@@ -202,5 +154,4 @@ maha_fem %>%
                 y = average + .13), 
             position = position_dodge(0.9),
             vjust = 0.5)
-
 
